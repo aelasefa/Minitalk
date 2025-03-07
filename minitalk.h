fbc00896e1312840b786f2fa0530776a332829b8
Ayman Elasefar <ayelasef@e2r10p7.1337.ma>
2025-03-07 07:55:12 +0000

    header file

---
+++ minitalk.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ayelasef <ayelasef@1337.ma>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/07 07:43:32 by ayelasef          #+#    #+#             */
+/*   Updated: 2025/03/07 07:43:50 by sboukiou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include "ft_printf/ft_printf.h"
+#include <signal.h>
+
+int	ft_atoi(char *str);
+int	ft_isdigit(int c);
+int	check_digit(char *str);
