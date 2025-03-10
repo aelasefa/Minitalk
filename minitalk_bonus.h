fe58fb9c4f36807cfdaf63f13ea6ff1969109d5d
Ayman Elasefar <ayelasef@e2r10p7.1337.ma>
2025-03-10 00:43:46 +0000

    header file

--- minitalk_bonus.h
+++ minitalk_bonus.h
@@ -1,17 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minitalk.h                                         :+:      :+:    :+:   */
+/*   minitalk_bonus.h                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ayelasef <ayelasef@1337.ma>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/07 07:43:32 by ayelasef          #+#    #+#             */
-/*   Updated: 2025/03/10 00:00:51 by ayelasef         ###   ########.fr       */
+/*   Updated: 2025/03/10 00:00:54 by ayelasef         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MINITALK_H
-# define MINITALK_H
+#ifndef MINITALK_BONUS_H
+# define MINITALK_BONUS_H
 # include "ft_printf/ft_printf.h"
 # include <signal.h>
 # include <stdio.h>
